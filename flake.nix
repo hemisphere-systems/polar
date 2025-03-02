77a5d9a33c7303aef27a069100caeba4e0be7ff8
Mara Schulke <mara.schulke@helsing.ai>
2025-03-02 09:48:02 +0100

    Setup flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    crane.url = "github:ipetkov/crane";
+
+    rust = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      utils,
+      rust,
+      crane,
+      nixpkgs,
+      ...
+    }@inputs:
+
+    utils.lib.eachDefaultSystem (
+      system:
+
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+
+        lib = nixpkgs.lib;
+
+        args = { inherit pkgs lib system; };
+
+        callPkg = import ./callPkg.nix inputs;
+
+        packages = import ./pkgs inputs (args // { inherit callPkg; });
+        checks = utils.lib.flattenTree packages;
+      in 
+      {
+        inherit packages checks;
+      }
+    )
+    // {
+      overlays.default = import ./overlay inputs { inherit (nixpkgs) lib; };
+      lib = import ./lib inputs;
+    };
+}
