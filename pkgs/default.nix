01666e6faef8483ccd7bfd8ffa4b4ee30a811078
Mara Schulke <mail@maras.cloud>
2025-10-12 16:50:34 +0200

    Add discord

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,7 +5,7 @@ let
   unfree = inputs.unfree.legacyPackages.${pkgs.system};
 in
 {
-  inherit (unfree) _1password-gui _1password-cli;
+  inherit (unfree) _1password-gui _1password-cli discord;
 
   cargo-pgrx = import ./cargo-pgrx.nix inputs args;
 }
